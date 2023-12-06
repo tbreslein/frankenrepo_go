b0207bea11cb67f8c4fb61650dca15e102703bff
Tommy Breslein <tommy.breslein@anacision.de>
2023-12-06 13:50:37 +0100

    fix/flake/add rust and c/cpp build tools

--- flake.nix
+++ flake.nix
@@ -3,15 +3,37 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [ (import rust-overlay) ];
+        #pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system overlays; };
         version = "0.0.1";
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ go gopls gofumpt golangci-lint go-task ];
+          buildInputs = with pkgs; [
+            go
+            gopls
+            gofumpt
+            golangci-lint
+            go-task
+
+            # for rust test projects
+            openssl
+            pkg-config
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-analyzer" ];
+            })
+
+            # for c and c++ test projects
+            cmake
+            gnumake
+            ninja
+            gcc
+          ];
         };
 
         packages.default = pkgs.buildGoModule {
