ac057054b45185307e5ef88e1f0b27aa84dd246f
Tommy Breslein <tommy.breslein@protonmail.com>
2024-03-28 17:30:40 +0100

    replace flake-utils with flake-parts

--- flake.nix
+++ flake.nix
@@ -1,47 +1,54 @@
 {
   description = "language/toolchain agnostic monorepo tool";
+
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        #pkgs = nixpkgs.legacyPackages.${system};
-        pkgs = import nixpkgs { inherit system overlays; };
-        version = "0.0.1";
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            go
-            gopls
-            gofumpt
-            golangci-lint
-            go-task
 
-            # for rust test projects
-            openssl
-            pkg-config
-            (rust-bin.stable.latest.default.override {
-              extensions = [ "rust-analyzer" ];
-            })
+  outputs = inputs@{ self, ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-            # for c and c++ test projects
-            cmake
-            gnumake
-            ninja
-            gcc
-          ];
+      perSystem = { system, pkgs, ... }:
+        let version = "0.0.1";
+        in {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (import inputs.rust-overlay) ];
+          };
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go
+              gopls
+              gofumpt
+              golangci-lint
+              just
+
+              # for rust test projects
+              openssl
+              pkg-config
+              (rust-bin.stable.latest.default.override {
+                extensions = [ "rust-analyzer" ];
+              })
+
+              # for c and c++ test projects
+              cmake
+              gnumake
+              ninja
+              gcc
+            ];
+          };
+
+          packages.default = pkgs.buildGoModule {
+            pname = "frankenrepo";
+            inherit version;
+            src = ./.;
+            # vendorSha256 = "0000000000000000000000000000000000000000000000000000";
+            vendorSha256 = "3tO/+Mnvl/wpS7Ro3XDIVrlYTGVM680mcC15/7ON6qM=";
+          };
         };
-
-        packages.default = pkgs.buildGoModule {
-          pname = "frankenrepo";
-          inherit version;
-          src = ./.;
-          # vendorSha256 = "0000000000000000000000000000000000000000000000000000";
-          vendorSha256 = "3tO/+Mnvl/wpS7Ro3XDIVrlYTGVM680mcC15/7ON6qM=";
-        };
-      });
+    };
 }
